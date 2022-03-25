82984a08ebdb1fac3c48460de73897baa38590ef
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-25 23:27:28 +0100

    add basic path param

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get('/')
 def index():
     return({'message':'welcome to fastAPI'})
+
+
+@app.get('/about')
+def about():
+    return ({'data':{ 'message': 'The about page'}})
+
+
+@app.get('/blog/{id}')
+def get_post(id : int):
+    return ({'data':{'message':f'your path param is {id}'}})
