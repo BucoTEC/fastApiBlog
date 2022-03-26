dd08995fcda35fcd4bfa2e756d86a236472869f5
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-26 10:01:43 +0100

    parsed first req model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -24,3 +25,12 @@ def method_name(limit: int = 10, sort : Optional[str] = None):
     if sort:
         return ({'message': f'your sort is {sort}'})
     return({'message': f'your limit is {limit}'})
+
+class Blog_post(BaseModel):
+    title : str
+    body: str
+    published_at : Optional[bool]
+
+@app.post('/podatci')
+def podatci(blog : Blog_post):
+    return {'message': f'ovo je stranica sa podatcima, vas title je {blog.title}'}
