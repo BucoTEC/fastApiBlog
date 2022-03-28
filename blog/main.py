695e65402b10e92a0dc226831a57dc73a2c842bf
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-29 00:27:04 +0200

    fix python interpreter and pipenv

--- blog/main.py
+++ blog/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from . import schemas
-from . import models, db
+from .schemas import Blog 
+from . import models
+from .db import engine
 
-models.Base.metadata.create_all(db.engine)
+models.Base.metadata.create_all(engine)
 app = FastAPI()
 
 
 
 @app.post('/blog')
-def create( body : schemas.Blog):
+def create( body : Blog):
     return {'data':{'title': body.title, 'body':body.body}}
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,12 @@
 from operator import index
 from turtle import title
 from sqlalchemy import Column,Integer,String
-from db import Base
+from .db import Base
 
 class User(Base):
+
+    __tablename__ = 'blog'
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
