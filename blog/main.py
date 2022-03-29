e9a5fc5ae5ba7a99af070bd6ed35f7a857d0974d
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-29 12:58:03 +0200

    add error handling

--- blog/main.py
+++ blog/main.py
@@ -37,15 +37,20 @@ def  show(id : int, db : Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def  destroy(id : int, db : Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id: {id} not found')
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'done'
 
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def  update(id : int, req : schemas.Blog, db : Session = Depends(get_db)):
-    print(req)
-    db.query(models.Blog).filter(models.Blog.id == id).update(dict(req))
+    blog =  db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id: {id} not found')
+    blog.update(dict(req))
     db.commit()
     return 'successful update'
 
