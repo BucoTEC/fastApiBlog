5f1fecf85b8653439c4ca7421ba2779f623cf197
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-29 12:53:24 +0200

    fix incomig req shape

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,6 @@
 from fastapi import Depends, FastAPI, status, HTTPException
 from sqlalchemy.orm import Session
-from .schemas import Blog 
+from . import schemas
 from . import models
 from .db import engine, SessionLocal
 
@@ -15,7 +15,7 @@ def get_db():
         db.close()
 
 @app.post('/blog', status_code=status.HTTP_201_CREATED)
-def create( req : Blog, db : Session = Depends(get_db)):
+def create( req : schemas.Blog, db : Session = Depends(get_db)):
     new_blog =  models.Blog(title=req.title, body=req.body )
     db.add(new_blog)
     db.commit()
@@ -31,8 +31,6 @@ def all(db : Session = Depends(get_db)):
 def  show(id : int, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).where(models.Blog.id == id).first()
     if not blog:
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return f'Blog by id of: {id} dose not exist'
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'blog of id:{id} not found')
     return blog
 
@@ -42,3 +40,12 @@ def  destroy(id : int, db : Session = Depends(get_db)):
     db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
     db.commit()
     return 'done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def  update(id : int, req : schemas.Blog, db : Session = Depends(get_db)):
+    print(req)
+    db.query(models.Blog).filter(models.Blog.id == id).update(dict(req))
+    db.commit()
+    return 'successful update'
+
