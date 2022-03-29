3f135eb3963e0a298df940232623e233a22d6e8d
Adnan Bucalovic <adnan.bucalovic13@gmail.com>
2022-03-29 12:28:16 +0200

    add custom response code

--- blog/main.py
+++ blog/main.py
@@ -1,5 +1,4 @@
-from pyexpat import model
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, status, Response
 from sqlalchemy.orm import Session
 from .schemas import Blog 
 from . import models
@@ -15,7 +14,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create( req : Blog, db : Session = Depends(get_db)):
     new_blog =  models.Blog(title=req.title, body=req.body )
     db.add(new_blog)
@@ -28,7 +27,10 @@ def all(db : Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
-def  show(id : int, db : Session = Depends(get_db)):
+@app.get('/blog/{id}', status_code=200)
+def  show(id : int,response : Response, db : Session = Depends(get_db)):
     blog = db.query(models.Blog).where(models.Blog.id == id).first()
+    if not blog:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return f'Blog by id of: {id} dose not exist'
     return blog
